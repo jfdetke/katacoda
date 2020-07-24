dbb3c31b2b15f7d16b98212a33ebb72b00dbed6f
john detke <jdetke@gmail.com>
2020-07-24 11:09:42 -0700

    terraform fmt

--- k8s/Terraform/terraform-cloud/main.tf
+++ k8s/Terraform/terraform-cloud/main.tf
@@ -1,14 +1,14 @@
 variable "stage" {
-    default = "production"
+  default = "production"
 }
 
 resource "random_pet" "server" {
-    keepers = {
-        # generate new pet name, each time we switch stage
-        stage = "${var.stage}"
-    }
+  keepers = {
+    # generate new pet name, each time we switch stage
+    stage = "${var.stage}"
+  }
 }
 
 output "random_server_id" {
-    value = random_pet.server.id
+  value = random_pet.server.id
 }
